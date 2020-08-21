a7dd91b4e9d46d22905a6f2b422638465f687b04
hayashiki <masayuki@hayashida.dev>
2020-08-21 22:40:39 +0900

    Add ref

--- timestamp.go
+++ timestamp.go
@@ -2,6 +2,7 @@
 //
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+// copied from https://github.com/google/go-github/blob/master/github/timestamp.go
 
 package docbase
 
